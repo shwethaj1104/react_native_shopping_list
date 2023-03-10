c8f41f433c184f3e5fad12592085d55694e293b5
Shwethaj1104 <shwethaj1104@gmail.com>
2023-03-10 14:08:06 +0530

    Added debug-->launch.json

--- salesApp/App.js
+++ salesApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome</Text>
+      <Text>Welcome here</Text>
       <StatusBar style="auto" />
     </View>
   );
