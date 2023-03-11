b1f53941869907d4f678da55ab8220e07cc3813b
Shwethaj1104 <shwethaj1104@gmail.com>
2023-03-11 11:17:10 +0530

    components : image, button, safeAreaView for ios --> platform module for android

--- salesApp/App.js
+++ salesApp/App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView, Image, Button, Alert,Platform } from 'react-native';
 
 export default function App() {
+  const handlePress=()=>{
+    console.log("text click")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Welcome here</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Welcome</Text>
+      {/* <Image source={require('./assets/favicon.png')}/> */}
+      <Image fadeDuration={1000} source={{
+        width:200,
+        height:200,
+        uri:"https://picsum.photos/200/300"}}/>
+
+        <Button title='ALERT' 
+        onPress={()=>
+        Alert.alert("Mytitle", "my message", [
+          {text:"Yes",onPress:()=>console.log("Yes")},
+          {text:"No",onPress:()=>console.log("No")}
+        ])}>
+        </Button>
+
+        {/* works only on ios */}
+        <Button title='prompt' onPress={()=>
+        Alert.prompt("Mytitle", "my message", text=>console.log("prompted"))}></Button>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +35,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent:'center',
+    alignItems:'center',
+    paddingTop:Platform.OS==='android' ? 20 : 0
   },
 });
